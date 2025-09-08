c0e9fe58b3ccb85dc5d1ad43f25bc125aeaab9a7
hinata9276 <lyy1992@gmail.com>
2025-09-08 15:52:32 +0800

    Update and rename MUX4_MUX8.v to MUX4_8_16.v
    added MUX16 primitive

--- verilog/MUX4_8_16.v
+++ verilog/MUX4_8_16.v
@@ -16,6 +16,7 @@ module mux4 (
 
 endmodule
 
+//module only applicable to Xilinx FPGA because only its CLB has MUXF7/F8 primitives
 module mux8 (
     input [7:0] data,    // 8-bit input data (D0 to D7)
     input [2:0] select,  // 3-bit select input (S2, S1, S0)
@@ -43,5 +44,33 @@ module mux8 (
         .I1(mux4_out1),  // Input (tie to LUT6 O6 pin)
         .S(select[2])     // Input select to MUX
     );
+endmodule
 
+module mux16 (
+    input [15:0] data,    // 8-bit input data (D0 to D7)
+    input [3:0] select,  // 3-bit select input (S2, S1, S0)
+    output out           // Output of the multiplexer
+);
+    wire mux8_out0;  // Output from the first MUX4
+    wire mux8_out1;  // Output from the second MUX4
+
+    // Instantiate two MUX4 modules
+    mux8 mux8_0 (
+        .data(data[7:0]),         // Lower 4 inputs (D0 to D3)
+        .select(select[2:0]),     // Least significant select bits
+        .out(mux8_out0)
+    );
+
+    mux8 mux8_1 (
+        .data(data[15:8]),         // Upper 4 inputs (D4 to D7)
+        .select(select[2:0]),     // Least significant select bits
+        .out(mux8_out1)
+    );
+
+    MUXF8 MUXF8_inst (
+        .O(out),    // Output of MUX to general routing
+        .I0(mux8_out0),  // Input (tie to LUT6 O6 pin)
+        .I1(mux8_out1),  // Input (tie to LUT6 O6 pin)
+        .S(select[3])     // Input select to MUX
+    );
 endmodule
